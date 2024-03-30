0ed56cc64761a5d694ba70d52bb35225bcefec50
betiiy-haile <betelhemhaile74@gmail.com>
2024-03-30 17:18:27 +0300

    build to production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Thought Share | share your thoughts with the world",
